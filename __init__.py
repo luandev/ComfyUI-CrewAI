1394bca920afadf60ab8cfa5f3d0e12160de3166
Luan Citta <dev.luan.citta@gmail.com>
2024-06-05 10:22:24 +0100

    rearange files

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
-from .textview_node import TextViewNode
-from .agent_node import AgentNode
-from .task_node import TaskNode
-from .crew_node import CrewNode
-from .list_task_node import TaskList
-from .list_agent_node import AgentList
+# from .textview_node import TextViewNode
+from .nodes.agent_node import AgentNode
+from .nodes.task_node import TaskNode
+from .nodes.crew_node import CrewNode
+from .nodes.list_task_node import TaskList
+from .nodes.list_agent_node import AgentList
 
 NODE_CLASS_MAPPINGS = {
     "📎CrewAI Agent": AgentNode,
